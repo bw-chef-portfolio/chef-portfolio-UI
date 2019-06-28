5917db23f9862345aa28ca6919ed33cfd78604e4
dannyvidal <vidaldanny94@gmail.com>
2019-06-28 10:44:06 -0400

    linked about button to registration page

---
+++ javascript/about.js
@@ -0,0 +1,5 @@
+let aboutButton = document.querySelector(".about-button")
+
+aboutButton.addEventListener("click", () => {
+    window.location.href = 'https://chef-portfolio-bw.netlify.com/registration';
+})
